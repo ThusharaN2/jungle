1624d49d403e455e001423623adf15064cd53f42
Thushara <thusharan@hotmail.com>
2022-06-06 17:04:27 -0400

    fix bug with admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,5 @@
 class Admin::ProductsController < ApplicationController
-
-  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
 
 
   def index
