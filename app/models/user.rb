487bef5f5529a9dd98327224c4c2bef409c4eba0
Thushara <thusharan@hotmail.com>
2022-06-08 15:36:53 -0400

    authenticate user in sessions_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+  validates :password, presence: true, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email)
+    if  user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
 end
