797a3c8d6ad5b3e4eb8f05e0c836b5ad00f92608
Thushara <thusharan@hotmail.com>
2022-06-07 21:06:30 -0400

    implement user authorization

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,12 @@ Rails.application.routes.draw do
 
   end
 
+  post '/users' => 'users#create'
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
