b07add85c1620ae27fd454a6db39a6e3740f7372
Thushara <thusharan@hotmail.com>
2022-06-07 23:42:21 -0400

    implement rspec testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_035367) do
+ActiveRecord::Schema.define(version: 2022_06_08_033916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,14 +52,11 @@ ActiveRecord::Schema.define(version: 2022_06_07_035367) do
     t.bigint "category_id"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
-  
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
+
+  create_table "widgets", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
